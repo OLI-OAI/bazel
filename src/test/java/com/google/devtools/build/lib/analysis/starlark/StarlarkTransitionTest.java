9a8a86572bf5f684fd55974996bfa52ffbd0f77a
nharmata <nharmata@google.com>
2026-08-12 11:32:56 -0700

    Intern `StarlarkBuildSettingsDetailsValue.Key` via `SkyKeyInterner`.
    Implement `SkyKey` interning for `StarlarkBuildSettingsDetailsValue.Key` to prevent duplicate key records and underlying collections in Skyframe transition evaluation.
    
    Previously, evaluating configuration transitions in `TransitionApplier` repeatedly instantiated brand-new `StarlarkBuildSettingsDetailsValue.Key` instances and wrapper `SingletonImmutableSet` collections for Starlark build setting labels. This caused duplicate keys and sets to be retained directly in Skyframe node dependency arrays.
    
    This fix also addresses a related memory tax when using `--flag_alias`.
    
    PiperOrigin-RevId: 963565595
    Change-Id: I3728144edef4070c54560b0c2129aefaaed3fbcd
    (cherry picked from commit a767149c8061e81be9cc2136ab77c99189aa8490)

--- src/test/java/com/google/devtools/build/lib/analysis/starlark/StarlarkTransitionTest.java
+++ src/test/java/com/google/devtools/build/lib/analysis/starlark/StarlarkTransitionTest.java
@@ -18,6 +18,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.analysis.ConfiguredRuleClassProvider;
 import com.google.devtools.build.lib.analysis.ConfiguredTarget;
@@ -1001,6 +1002,17 @@ public class StarlarkTransitionTest extends BuildViewTestCase {
     assertThat(t3NonExecConfig.getOptions().getStarlarkOptions())
         .containsExactly(
             Label.parseCanonicalUnchecked("//test:foo_starlark"), "transitioned cmd_value");
+  }
+
+  @Test
+  public void starlarkBuildSettingsDetailsValueKey_isInterned() {
+    Label label1 = Label.parseCanonicalUnchecked("//test:flag");
+    Label label2 = Label.parseCanonicalUnchecked("//test:flag");
+    StarlarkBuildSettingsDetailsValue.Key key1 =
+        StarlarkBuildSettingsDetailsValue.key(ImmutableSet.of(label1));
+    StarlarkBuildSettingsDetailsValue.Key key2 =
+        StarlarkBuildSettingsDetailsValue.key(ImmutableSet.of(label2));
+    assertThat(key1).isSameInstanceAs(key2);
   }
 
   private ImmutableList<ConfiguredTarget> getComputedConfiguredTarget(String label)
